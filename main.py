9571f80f610c9da7810661e7251e78ee07d30929
buyanthg <buiantonbui"gmail.com>
2023-04-12 01:29:36 +0300

    НАЧАЛО

---
+++ main.py
@@ -0,0 +1,12 @@
+def calc(math_expression: str):
+    res = 0
+    x = ''
+    operation = ''
+    for i in math_expression:
+        if i.isdigit():
+            x += i
+        else:
+            res = int
+            operation = i
+
+    return n
